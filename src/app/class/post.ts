70520749c21c7263992d0e264f92608b3a5896bb
t-pietrzak <tpietrzak4@gmail.com>
2020-10-21 21:14:50 +0200

    post class and create method

--- src/app/class/post.ts
+++ src/app/class/post.ts
@@ -1,2 +1,6 @@
 export class Post {
+    key: string;
+    title: string;
+    description = Date.now();
+    published = false
 }
--- src/app/service/actions.service.ts
+++ src/app/service/actions.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { Post } from '../class/post'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActionsService {
 
-  constructor() { }
+  private dbPath = '/posts';
+
+  postRef: AngularFireList<Post> = null;
+
+  constructor(private db: AngularFireDatabase) {
+    this.postRef = db.list(this.dbPath);
+  }
+
+  create(post: Post): any {
+    return this.postRef.push(post);
+  }
 }
